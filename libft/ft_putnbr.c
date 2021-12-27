a2fde4196269e2bba460e127ec27af7272a6ab1d
wrolanda <Nikita.Mitriackov@gmail.com>
2021-12-28 02:19:41 +0300

    libft's functions

---
+++ libft/ft_putnbr.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 13:17:28 by wrolanda          #+#    #+#             */
+/*   Updated: 2021/12/28 01:45:17 by wrolanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+int	ft_countnbr(int n)
+{
+	int	i;
+
+	i = 1;
+	if (n < 0)
+	{
+		i++;
+		if (n == -2147483648)
+		{
+			i++;
+			n = -147483648;
+		}
+		n = -n;
+	}
+	while (n > 9)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
+
+int	ft_putnbr(int n)
+{
+	int	k;
+
+	k = ft_countnbr(n);
+	if (n < 0)
+	{
+		ft_putchar('-');
+		if (n == -2147483648)
+		{
+			ft_putchar('2');
+			n = -147483648;
+		}
+		n = -n;
+	}
+	if (n < 10)
+		ft_putchar(n + '0');
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putchar((n % 10) + '0');
+	}
+	return (k);
+}
+
+/*#include <stdio.h>
+int	main(void)
+{
+	int	i =  -2147483648;
+	int k = 0;
+	k = ft_putnbr(i);
+	printf("\n\n%d", k);	
+}*/
---
+++ libft/ft_putstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 12:46:28 by wrolanda          #+#    #+#             */
+/*   Updated: 2021/12/28 01:36:29 by wrolanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+int	ft_putstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s == NULL)
+		return (0);
+	while (s[i])
+		i += ft_putchar(s[i]);
+	return (i);
+}
+
+/*int	main(void)
+{
+	int	i1 = 0;
+	//int	i2 = 0;
+	char	*res;
+	
+	while (i1++ < 10)
+	{
+		res = ft_itoa(i1);
+		write (1, &res[0], 1);
+	}
+}*/
